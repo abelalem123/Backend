692e11bb356b795e3ab61f8329b0c3128485c23d
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2022-12-01 11:10:55 +0300

    Backend is integrated with MongoDB

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ console.log(error);
 if (error.name === 'CastError') {
   return response.status(400).send({ error: 'malformatted id' })
 } 
-else if(error.name==='Validation Error'){
-  return response.status(400).json({error:error.message})
+else if (error.name === 'ValidationError') {
+  return response.status(400).json({ error: error.message })
 }
 next(error)
 }
